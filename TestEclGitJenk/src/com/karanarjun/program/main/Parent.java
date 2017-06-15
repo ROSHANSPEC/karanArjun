fb1b8c9a7bcb4001425a0890dc6c5c49bcbb2d1b
Roshan <My robo@Roshan>
2017-06-16 01:47:07 +0530

    commit-001

---
+++ TestEclGitJenk/src/com/karanarjun/program/main/Parent.java
@@ -0,0 +1,13 @@
+package com.karanarjun.program.main;
+
+public class Parent {
+	
+	public static void printMessage(){
+		System.out.println("hello world");
+	}
+	
+	public static void main(String[] args) {
+		printMessage();
+	}
+
+}
